4eb854ec62481cf6b71230d3eabadbc2e85391f8
victorimannuel <imannuelvictor1878@gmail.com>
2022-08-01 12:34:17 +0700

    minor update

--- resources/views/main/components/table-penduduk.blade.php
+++ resources/views/main/components/table-penduduk.blade.php
@@ -23,7 +23,6 @@
     </div>
     <div class="fs-4">
         <form action="/search" method="POST" role="search">
-{{--                    <form action="/dashboard/search" method="POST" role="search">--}}
             <div class="row w-100 justify-content-evenly">
                 @csrf
                 <div class="col-md-3 w-auto h-auto">
@@ -62,63 +61,61 @@
                     </div>
                 </div>
             </div>
-        {{-- </form> --}}
+        </form>
 
-<div class="table-responsive py-5">
-    <table id="list-penduduk" class="table table-hover">
-        <thead class="h4">
-        <tr>
-            <th scope="col" class="text-center pe-3"><b>No</b></th>
-            <th scope="col">Nomor KK</th>
-            <th scope="col">Nama</th>
-            <th scope="col">NIK</th>
-            <th scope="col">RT</th>
-            <th scope="col">RW</th>
-            <th scope="col">Dusun</th>
-            <th scope="col">Validasi</th>
-            <th scope="col"></th>
-        </tr>
-        </thead>
-        <tbody>
-        @php $i = 1; @endphp
-        @foreach($penduduks as $penduduk)
-            <tr >
-                <td class="text-center pe-3"><b>{{ $i }}</b></td>
-                <td >{{ $penduduk->no_kk }}</td>
-                <td >{{ $penduduk->nama }}</td>
-                <td >{{ $penduduk->nik  }}</td>
-                <td >{{ $penduduk->rt }}</td>
-                <td >{{ $penduduk->rw }}</td>
-                <td >{{ $penduduk->dusun }}</td>
-                <td >{{ $penduduk->validasi }}</td>
-                <td >
+        <div class="table-responsive py-5">
+            <table id="list-penduduk" class="table table-hover">
+                <thead class="h4">
+                <tr>
+                    <th scope="col" class="text-center pe-3"><b>No</b></th>
+                    <th scope="col">Nomor KK</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">NIK</th>
+                    <th scope="col">RT</th>
+                    <th scope="col">RW</th>
+                    <th scope="col">Dusun</th>
+                    <th scope="col">Validasi</th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                @php $i = 1; @endphp
+                @foreach($penduduks as $penduduk)
+                    <tr >
+                        <td class="text-center pe-3"><b>{{ $i }}</b></td>
+                        <td >{{ $penduduk->no_kk }}</td>
+                        <td >{{ $penduduk->nama }}</td>
+                        <td >{{ $penduduk->nik  }}</td>
+                        <td >{{ $penduduk->rt }}</td>
+                        <td >{{ $penduduk->rw }}</td>
+                        <td >{{ $penduduk->dusun }}</td>
+                        <td >{{ $penduduk->validasi }}</td>
+                        <td >
 
-                    <button type="button" class="btn btn-lg btn-dark"
-                            onclick="window.location='{{ route('view-penduduk.show', ['id_penduduk' => $penduduk->id])}}'"
-                    >
-                        Detail
-                    </button>
+                            <button type="button" class="btn btn-lg btn-dark"
+                                    onclick="window.location='{{ route('view-penduduk.show', ['id_penduduk' => $penduduk->id])}}'"
+                            >
+                                Detail
+                            </button>
 
-                </td>
-            </tr>
-            @php $i += 1; @endphp
-        @endforeach
-        </tbody>
-    </table>
+                        </td>
+                    </tr>
+                    @php $i += 1; @endphp
+                @endforeach
+                </tbody>
+            </table>
 
+        </div>
+
+        {{ $penduduks->links() }}
+
+
+        <script>
+            document.getElementById('pagination').onchange = function() {
+                window.location = "{!! $penduduks->url(1) !!}&items=" + this.value;
+            };
+        </script>
+
+
+    </div>
 </div>
-
-{{ $penduduks->links() }}
-
-
-<script>
-    document.getElementById('pagination').onchange = function() {
-        window.location = "{!! $penduduks->url(1) !!}&items=" + this.value;
-    };
-</script>
-
-
-
-</div>
-</div>
-</section>

d809868eaee9c10218484bc05a7e8fcadf53aa0f
zoomy-git <ahmadrolandi01@gmail.com>
2022-08-02 21:01:59 +0700

    fiishing statistik page

--- resources/views/main/charts/jenisKelaminChart.blade.php
+++ resources/views/main/charts/jenisKelaminChart.blade.php
@@ -1,44 +1,46 @@
 <div class="card mx-5 p-5">
     <h1 class="text-center"><b>Jenis Kelamin</b></h1>
-    <canvas id="jenisKelaminChart" class="p-5" width=400 height=400></canvas>
+    <canvas id="jenisKelaminChart" class="m-5 p-4 border rounded" width=400 height=400></canvas>
     
-  </div>
-  <script>
-    const ctx = document.getElementById('jenisKelaminChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Laki - Laki', 'Perempuan'],
-            datasets: [{
-                label: 'Penduduk',
-                data: [
-                  {{ '1000' }},
-                  {{ '600' }}
-                ],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                ],
-                borderWidth: 1
-            }]
+</div>
+<script>
+  const jenisKelamin = document.getElementById('jenisKelaminChart').getContext('2d');
+  const jenisKelaminChart = new Chart(jenisKelamin, {
+      type: 'bar',
+      data: {
+          labels: ['Laki - Laki', 'Perempuan'],
+          datasets: [{
+              label: 'Penduduk',
+              data: [
+                {{ '1000'  }},  /* data laki */
+                {{ '600'  }}    /* data cw   */
+              ],
+              backgroundColor: [
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+              ],
+              borderColor: [
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        legend: {
+          display: false
         },
-        options: {
-          legend: {
-            display: false
-          },
-          title: {
-            display: true,
-            text: "Jenis Kelamin"
-          },
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-    </script>
+        title: {
+          display: true,
+          text: "Jenis Kelamin"
+        },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  </script>
---
+++ resources/views/main/charts/umurChart.blade.php
@@ -0,0 +1,53 @@
+<div class="card mx-5 p-5">
+    <h1 class="text-center"><b>Umur</b></h1>
+    <canvas id="umurChart" class="m-5 p-4 border rounded" width=400 height=400></canvas>
+    
+</div>
+<script>
+  const umur = document.getElementById('umurChart').getContext('2d');
+  const umurChart = new Chart(umur, {
+      type: 'pie',
+      data: {
+          labels: ['Balita', 'Anak-Anak', 'Remaja', 'Dewasa', 'Lansia', 'Manula'],
+          datasets: [{
+              label: 'Penduduk',
+              data: [
+                {{ '1000'  }},  /* data balita */
+                {{ '600'  }},   /* data anak2  */
+                {{ '1000'  }},  /* data remaja */
+                {{ '600'  }},   /* data dewasa  */
+                {{ '1000'  }},  /* data lansia */
+                {{ '600'  }},   /* data manula  */
+              ],
+              backgroundColor: [
+                'rgb(105, 180, 102)',
+                'rgb(42, 76, 151)',
+                'rgb(253, 159, 97)',
+                'rgb(252, 221, 92)',
+                'rgb(234, 110, 120)',
+                'rgb(128, 41, 92)'
+              ],
+            hoverOffset: 4
+          }]
+      },
+      options: {
+        tooltips: {
+            enabled: false
+        },
+        plugins: {
+            datalabels: {
+                formatter: (value, umur) => {
+                    let sum = 0;
+                    let dataArr = umur.chart.data.datasets[0].data;
+                    dataArr.map(data => {
+                        sum += data;
+                    });
+                    let percentage = (value*100 / sum).toFixed(2)+"%";
+                    return percentage;
+                },
+                color: '#fff',
+            }
+        }
+      }
+  });
+  </script>

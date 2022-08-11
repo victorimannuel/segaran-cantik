ea919fa8a802105905100031ade2f5462c0cd34e
zoomy-git <ahmadrolandi01@gmail.com>
2022-08-12 06:25:30 +0700

    finishing export/import page responsive

--- resources/views/main/export-import.blade.php
+++ resources/views/main/export-import.blade.php
@@ -23,33 +23,39 @@
                         File berhasil diimport
                         {{Session::get('success')}}
                     </div>
-                    <div class="container d-flex flex-row justify-content-between w-100 align-items-between p-0">
-
-                        <div class="me-5 w-50 p-5 card d-flex justify-content-between" >
-                            <div class="text">
-                                <h1><b>Export</b></h1>
-                                <p class="fs-3 my-4">
-                                    Untuk memastikan data yang telah diisikan di sini tidak hilang, anda bisa mendownload data tersebut dengan klik di bawah ini
-                                </p>
+                    <div class="container d-flex flex-row justify-content-between w-100 align-items-between p-0 row">
+                        <div class="col-md card-container my-5">
+                            <div id="export" class="p-5 card export-card" >
+                                <div class="text">
+                                    <h1><b>Export</b></h1>
+                                    <p class="fs-3 my-4">
+                                        Untuk memastikan data yang telah diisikan di sini tidak hilang, anda bisa mendownload data tersebut dengan klik di bawah ini
+                                    </p>
+                                </div>
+                                <a class="btn btn-success btn-lg fs-3 my-4" href="{{ route('export-penduduk') }}">Export Data Penduduk</a>
                             </div>
-                            <a class="btn btn-success btn-lg fs-3 my-4" href="{{ route('export-penduduk') }}">Export Data Penduduk</a>
+
+                        </div>
+
+                        <div class="col-md card-container my-5">
+                            <div id="import" class="p-5 col-md card export-card" >
+                                <div class="text">
+                                    <h1><b>Import</b></h1>
+                                    <p class="fs-3 my-4">Untuk mempermudah dalam penginputan data, anda dapat mengimport data tersebut di bawah ini
+                                    </p>
+    
+                                </div>
+            
+                                <div class="form-group d-flex flex-column justify-content-center">
+                                    <label for="customFile" class="form-label text-start fs-4">Pilih file Excel atau CSV</label>
+                                    <input class="form-control fs-4 form-control-lg " type="file" id="customFIle" name="file">
+                                </div>
+                                <button class="btn btn-primary btn-lg fs-3 my-4">Import Data Penduduk</button> 
+                            </div>
+
                         </div>
     
     
-                        <div class="ms-5 w-50 p-5 card d-flex justify-content-between" >
-                            <div class="text">
-                                <h1><b>Import</b></h1>
-                                <p class="fs-3 my-4">Untuk mempermudah dalam penginputan data, anda dapat mengimport data tersebut di bawah ini
-                                </p>
-
-                            </div>
-        
-                            <div class="form-group d-flex flex-column justify-content-center">
-                                <label for="customFile" class="form-label text-start fs-4">Pilih file Excel atau CSV</label>
-                                <input class="form-control fs-4 form-control-lg " type="file" id="customFIle" name="file">
-                            </div>
-                            <button class="btn btn-primary btn-lg fs-3 my-4">Import Data Penduduk</button> 
-                        </div>
                     </div>
                 </form>
             </section>
